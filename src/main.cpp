649851a3cc021fcb3ca8a47972347c923845056b
akira0127 <red.wave.noa@gmail.com>
2023-09-22 00:28:32 +0900

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <M5CoreS3.h>
+#include <SoftwareSerial.h>
+
 // put function declarations here:
 int myFunction(int, int);
 
@@ -7,13 +9,15 @@ void setup()
 {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
-
+  serial.begin(115200);
 }
 
 void loop()
 
 {
   // put your main code here, to run repeatedly:
+  serial.println("hello");
+  delay(5000);
 }
 
 // put function definitions here:
